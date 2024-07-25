11ea6937c1671d107d3d61bcca9a6114d5851266
Felipe De Andrade <felipe.deam98@gmail.com>
2024-07-25 14:19:05 -0300

    fix: Correção para deploy na Vercel

--- src/components/Marcas/Marca/index.tsx
+++ src/components/Marcas/Marca/index.tsx
@@ -1,9 +1,9 @@
-import React, { forwardRef } from 'react';
+import { forwardRef } from 'react';
 import style from './Marca.module.scss';
 import vtex from '../../../assets/img/logo.png';
 import { Link } from 'react-router-dom';
 
-const Marca = forwardRef<HTMLAnchorElement>((props, ref) => {
+const Marca = forwardRef<HTMLAnchorElement>((_, ref) => {
   return (
     <Link ref={ref} className={style.marca} to={'/'}>
       <img className={style.marca__imagem} src={vtex} width={171} height={171} alt="#" />
